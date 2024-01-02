d57abf6dc88e033f0c0f227b121b963c57dd8e06
Lucas Eng <lucasengnz@outlook.com>
2024-01-02 21:10:54 +1300

    Routing added
    Two new routes created -- / and /results using react-router-dom

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,37 +1,14 @@
 import React from "react";
 import Navigation from "./Navigation";
 import Test from "./Test";
-import { createTheme, ThemeProvider } from "@mui/material";
-import { lime, purple } from "@mui/material/colors";
+
 import KudosChart from "./KudosChart";
-
-/*const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#ff5722",
-    },
-    secondary: {
-      main: "#f50057",
-    },
-  },
-});*/
-
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#FC4C02",
-    },
-    secondary: purple,
-  },
-});
+import { Typography } from "@mui/material";
 
 function HomePage() {
   return (
     <>
-      <ThemeProvider theme={theme}>
-        <Navigation></Navigation>
-        <KudosChart />
-      </ThemeProvider>
+      <Typography variant="h4">Sign in with Strava to continue.</Typography>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,29 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import Test from "./Test.jsx";
 import Navigation from "./Navigation.jsx";
-import { ThemeProvider } from "styled-components";
 import HomePage from "./HomePage.jsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { createTheme, ThemeProvider } from "@mui/material";
+import { lime, purple } from "@mui/material/colors";
+import KudosChart from "./KudosChart.jsx";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#FC4C02",
+    },
+    secondary: purple,
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <HomePage></HomePage>
+  <BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <Navigation></Navigation>
+      <Routes>
+        <Route path="/" element={<HomePage />} exact />
+        <Route path="/results" element={<KudosChart />} exact />
+      </Routes>
+    </ThemeProvider>
+  </BrowserRouter>
 );
